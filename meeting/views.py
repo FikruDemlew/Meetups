6035064cc7b515d0d24c3bbb0325c4fbaf4e5f9c
Fikru Demlew <fikrudemlew.com>
2026-07-08 00:42:23 +0300

    add all the stylesheets

--- meeting/views.py
+++ meeting/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HTTPResponse, HttpResponse
+from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the meeting index.")
+    return render(request, 'meeting/index.html')    
