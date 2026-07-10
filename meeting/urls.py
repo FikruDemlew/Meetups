42cab93ec89da691ccff4d153889850ca8499590
Fikiru Demlew <fikrudemlew@gmail.com>
2026-07-10 15:59:14 +0300

    Add dynamic data

--- meeting/urls.py
+++ meeting/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('meeting/', views.index)
+    path('meeting/', views.index, name='aql-meetups'),
+    path('meeting/<slug:meetup_slug>/', views.meetup_detail, name='meetup_detail')
 ]
--- meeting/views.py
+++ meeting/views.py
@@ -3,4 +3,23 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'meeting/index.html')    
+    meetups = [
+        {'title': 'A First Meetup', 'location': 'New York', 'slug': 'a-first-meetup'},
+        {'title': 'A Second Meetup', 'location': 'Los Angeles', 'slug': 'a-second-meetup'}
+    ]
+    return render(request, 'meeting/index.html', {
+        'show_meetups': False,
+        'meetups': meetups
+    })    
+    
+def meetup_detail(request, meetup_slug):
+    print(meetup_slug)
+    selected_meetup = {
+        'title': 'A First Meetup',
+        'description': 'This is a first meetup!',
+    }
+    return render(request, 'meeting/meetup-details.html', {
+        'meetup_tittle': selected_meetup['title'],
+        'meetup_description': selected_meetup['description']
+    })
+    
