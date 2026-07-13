bbfbbf79deebc22f9861ba6f2dba8e72bb3b5869
Fikiru Demlew <fikrudemlew@gmail.com>
2026-07-13 23:39:00 +0300

    created a model and a relationship

--- meeting/views.py
+++ meeting/views.py
@@ -1,25 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Meeting
 # Create your views here.
 
 def index(request):
-    meetups = [
-        {'title': 'A First Meetup', 'location': 'New York', 'slug': 'a-first-meetup'},
-        {'title': 'A Second Meetup', 'location': 'Los Angeles', 'slug': 'a-second-meetup'}
-    ]
+    meetups = Meeting.objects.all()
     return render(request, 'meeting/index.html', {
         'show_meetups': False,
         'meetups': meetups
     })    
     
 def meetup_detail(request, meetup_slug):
-    print(meetup_slug)
-    selected_meetup = {
-        'title': 'A First Meetup',
-        'description': 'This is a first meetup!',
-    }
+    selected_meetup = Meeting.objects.get(slug=meetup_slug)
     return render(request, 'meeting/meetup-details.html', {
-        'meetup_tittle': selected_meetup['title'],
-        'meetup_description': selected_meetup['description']
+        'meetup_title': selected_meetup.title,
+        'meetup_description': selected_meetup.description
     })
     
